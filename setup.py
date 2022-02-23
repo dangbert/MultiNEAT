a7f69d45221edfa2965f64412a6fc72853f12131
Aart Stuurman <aartstuurman@hotmail.com>
2022-02-23 15:05:45 +0100

    version 0.7 (#9)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from skbuild import setup
 
 
 setup(name='multineat',
-      version='0.6',  # Update version in conda/meta.yaml as well
+      version='0.7',  # Update version in conda/meta.yaml as well
       packages=['multineat'],
       cmake_args=['-DGENERATE_PYTHON_BINDINGS:BOOL=ON', '-DCMAKE_BUILD_TYPE=Release'],
       install_requires=['numpy'],
